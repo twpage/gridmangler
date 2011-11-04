12117069c07b7ae0d26a3a5e550978763ab80614
Todd Page <twpage@gmail.com>
2011-11-04 06:34:36 -0400

    finalish/beta version working in chrome, new helper methods and object structure

--- gridmangler.js
+++ gridmangler.js
@@ -6,28 +6,89 @@
 // v 0.1
 // ---------------------------------
 
-var gridmangler = function ( ) {
+var gridmangler = function (canvas, tile_width, tile_height) {
     
-    // PRIVATE Closure variables + functions
+    // PRIVATE Closure variables 
     ///////////////////////////////////////////////////////////////////////////
+    var my_canvas, my_context, my_tile_width, my_tile_height, my_width, my_height;
+    var my_current_xy = {"x": -1, "y": -1};
+    var my_last_click_xy = {"x": -1, "y": -1};
+    var my_last_visit_xy = {"x": -1, "y": -1};
     
-    var my_canvas;
-    var my_context;
-    var tile_width, tile_height;
-    var width;
-    var height;
-    var current_xy = {"x": -1, "y": -1};
+    var that = {};
+    
+    // Init
+    ///////////////////////////////////////////////////////////////////////////
+    my_canvas = canvas;
+    my_context = my_canvas.getContext("2d");
+    
+    if ((tile_width <= 0) || (tile_height <= 0)) {
+	throw {
+	    name: 'InvalidGridSizeError',
+	    message: 'Tile width and height must be positive'
+	};
+    }
+    my_tile_width = tile_width;
+    my_tile_height = tile_height;
+    
+    my_width = my_canvas.width / my_tile_width;
+    if (my_width - Math.floor(my_width) !== 0) {
+	throw {
+	    name: 'InvalidGridSizeError',
+	    message: 'Canvas width and tile width must produce an integer'
+	};
+    }
+    
+    my_height = my_canvas.height / my_tile_height;
+    if (my_height - Math.floor(my_height) !== 0) {
+	throw {
+	    name: 'InvalidGridSizeError',
+	    message: 'Canvas height and tile height must produce an integer'
+	};
+    }
+    
+    // PRIVATE Closure methods
+    ///////////////////////////////////////////////////////////////////////////
     
     // these 3 functions will be overridden
     var my_gridEventMousedown = function ( ) {};
     var my_gridEventLeaveFocus = function ( ) {};
     var my_gridEventGainFocus = function ( ) {};
     
+    var invalidGridCoordinates = function (grid_xy, event_name) {
+    // will eventually be a wrapper for user-defined error function is necessary?
+	
+    };
+    
+        var isValidGridCoordinates = function (test_xy) {
+    // Returns true if a given {x:, y:} coordinate object is within the bounds of the canvas grid
+    
+	if ((test_xy.x < 0) || (test_xy.y < 0)) {
+	  return false;
+	} else if ((test_xy.x >= my_width) || (test_xy.y >= my_height)) {
+	  return false;
+	} else {
+	  return true;
+	}
+    };
+    
+    var isArray = function (my_value) {
+	if (my_value && typeof my_value === 'object' &&
+	    typeof my_value.length === 'number' &&
+	    !(my_value.propertyIsEnumerable('length'))) {
+	    
+	    return true;
+	} else {
+	    return false;
+	}
+    };
+
     var gridEventMousedown = function (grid_xy) {
     // wrapper for user-defined grid event function
     
 	if (isValidGridCoordinates(grid_xy)) {
 	    my_gridEventMousedown(grid_xy);
+	    my_last_click_xy = grid_xy;
 	} else {
 	    invalidGridCoordinates(grid_xy, "mousedown");
 	}
@@ -38,6 +99,7 @@ var gridmangler = function ( ) {
     
 	if (isValidGridCoordinates(grid_xy)) {
 	    my_gridEventLeaveFocus(grid_xy);
+	    my_last_visit_xy = grid_xy;
 	} else {
 	    invalidGridCoordinates(grid_xy, "leavefocus");
 	}
@@ -51,11 +113,6 @@ var gridmangler = function ( ) {
 	} else {
 	    invalidGridCoordinates(grid_xy, "gainfocus");
 	}
-    };
-    
-    var invalidGridCoordinates = function (grid_xy, event_name) {
-    // will eventually be a wrapper for user-defined error function is necessary?
-	;
     };
     
     var relMouseCoords = function (event){
@@ -73,36 +130,36 @@ var gridmangler = function ( ) {
 	    totalOffsetX += currentElement.offsetLeft;
 	    totalOffsetY += currentElement.offsetTop;
 	}
-	while(currentElement = currentElement.offsetParent)
+	while((currentElement = currentElement.offsetParent))
     
 	canvasX = event.pageX - totalOffsetX;
 	canvasY = event.pageY - totalOffsetY;
     
-	return {x:canvasX, y:canvasY}
-    }
+	return {x:canvasX, y:canvasY};
+    };
     HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;    
     
     var convertMouseCoordinatesToGrid = function(event) {
     // converts canvas mouse coordinates to an internal grid coordinate
     // some of this from http://www.quirksmode.org/js/events_properties.html
     
+	var coords, posx, posy, gridx, gridy;
 	coords = my_canvas.relMouseCoords(event);
 	posx = coords.x;
 	posy = coords.y;
 	
 	// find the grid square they just clicked in
-	gridx = ~~(posx / tile_size);
-	gridy = ~~(posy / tile_size);	
+	gridx = ~~(posx / my_tile_width);
+	gridy = ~~(posy / my_tile_height);	
 	
-	response = {"x": gridx, "y": gridy};
-	return response;
+	return {"x": gridx, "y": gridy};
     };
       
     var canvasOnMousedown = function (event) {
     // Converts mouse coords into grid coords and calls appropriate grid event
     // grid "mousedown" is essentially the same as a canvas mousedown with mapped coords
     
-	grid_xy = convertMouseCoordinatesToGrid(event);
+	var grid_xy = convertMouseCoordinatesToGrid(event);
 	gridEventMousedown(grid_xy);
     };
     
@@ -112,171 +169,193 @@ var gridmangler = function ( ) {
     // Grid "gainfocus" is called when the mouse moves into a new grid
     // -> Mouse movements that stay within the same grid *do not trigger grid events* <-
     
-	grid_xy = convertMouseCoordinatesToGrid(event);
-	if ((current_xy.x != grid_xy.x) || (current_xy.y != grid_xy.y)) {
-	    if (current_xy.x != -1) {
-	      gridEventLeaveFocus(current_xy);
+	var grid_xy = convertMouseCoordinatesToGrid(event);
+	if ((my_current_xy.x !== grid_xy.x) || (my_current_xy.y !== grid_xy.y)) {
+	    if (my_current_xy.x !== -1) {
+	      gridEventLeaveFocus(my_current_xy);
 	    }
-	    current_xy.x = grid_xy.x;
-	    current_xy.y = grid_xy.y;
+	    my_current_xy.x = grid_xy.x;
+	    my_current_xy.y = grid_xy.y;
 	    gridEventGainFocus(grid_xy);
-	}
-    };
-    
-    var isValidGridCoordinates = function (test_xy) {
-    // Returns true if a given {x:, y:} coordinate object is within the bounds of the canvas grid
-    
-	if ((test_xy.x < 0) || (test_xy.y < 0)) {
-	  return false;
-	} else if ((test_xy.x >= width) || (test_xy.y >= height)) {
-	  return false;
-	} else {
-	  return true;
 	}
     };
     
     // PUBLIC object + methods
     ///////////////////////////////////////////////////////////////////////////
-    return {
-        
-        init: function (canvas, given_tile_size) {
-	// initialize a given HTML5 Canvas element, 
-	// ensures canvas size is suitable for given tile size (e.g. integer number of X x Y tiles)
-	// (will throw exceptions if not)
-	
-            my_canvas = canvas;
-            my_context = my_canvas.getContext("2d");
-            
-            
-            tile_size = given_tile_size;
-	    if (tile_size <= 0) {
-		throw {
-		    name: 'InvalidGridSizeError',
-		    message: 'Tile size must be positive'
-		};
-	    }
-	    // TODO: Allow for init of rectangular tiles
-	    tile_width = tile_size;
-	    tile_height = tile_size;
+    
+    that.addGridEvent = function (event_name, event_fn) {
+    // Pass in user-defined functions to link with grid events
+    // functions should take a {x: , y: } coordinate object as its only argument
+    // events will not be triggered for invalid coordinates so functions shouldn't worry about that
+    
+	if (event_name === "mousedown") {
+	    my_gridEventMousedown = event_fn;
 	    
-	    width = my_canvas.width / tile_width;
-	    if (width - Math.floor(width) !== 0) {
-		throw {
-		    name: 'InvalidGridSizeError',
-		    message: 'Canvas width and tile width must produce an integer'
-		};
-	    }
+	} else if (event_name === "gainfocus") {
+	    my_gridEventGainFocus = event_fn;
 	    
-	    height = my_canvas.height / tile_height;
-	    if (height - Math.floor(height) !== 0) {
-		throw {
-		    name: 'InvalidGridSizeError',
-		    message: 'Canvas height and tile height must produce an integer'
-		};
-	    }
+	} else if (event_name === "leavefocus") {
+	    my_gridEventLeaveFocus = event_fn;
 	    
-            my_canvas.addEventListener("mousedown", canvasOnMousedown, false);
-            my_canvas.addEventListener("mousemove", canvasOnMousemove, false);
-        },
+	} else {
+	    throw {
+		name: 'InvalidGridEventError',
+		message: 'Grid event not recognized: ' + event_name
+	    };
+	}
 	
-	addGridEvent: function (event_name, event_fn) {
-	// Pass in user-defined functions to link with grid events
-	// functions should take a {x: , y: } coordinate object as its only argument
-	// events will not be triggered for invalid coordinates so functions shouldn't worry about that
+	return true;
+    };
 	
-	    if (event_name === "mousedown") {
-		my_gridEventMousedown = event_fn;
-		
-	    } else if (event_name === "gainfocus") {
-		my_gridEventGainFocus = event_fn;
-		
-	    } else if (event_name === "leavefocus") {
-		my_gridEventLeaveFocus = event_fn;
-		
-	    } else {
-		throw {
-		    name: 'InvalidGridEventError',
-		    message: 'Grid event not recognized: ' + event_name
-		};
-	    }
-	    
-	    return true;
-	},
+    that.getTileFromOffset = function (grid_xy, offset_xy) {
+    // grid addition/subtraction -- return a new grid coordinate based on a start point and an offset
+    // will return null for invalid grid points
+    
+	var new_xy;
 	
-	getSurroundingTiles: function (grid_xy, b_include_center) {
-	// returns an array of {x: , y: } coordinates surrounding a given (center) coordinate
-	// will optionally include the center if the 2nd argument is passed as true
-	// IGNORES invalid coordinates (e.g. off the map)
+	new_xy = {"x": grid_xy.x + offset_xy.x, "y": grid_xy.y + offset_xy.y};
 	
-	    var offset_tiles = [
-		{"x": -1, "y": -1}, {"x": -1, "y": 0},
-		{"x": -1, "y": 1}, {"x": 0, "y": -1},
-		{"x": 0, "y": 0}, {"x": 0, "y": 1},
-		{"x": 1, "y": -1}, {"x": 1, "y": 0},
-		{"x": 1, "y": 1}
-	    ];
-				 
-	    if (b_include_center === true) {
-		offset_tiles.push({"x": 0, "y": 0});
-	    }
-	    
-	    var surrounding_tiles = new Array();
-	    
-	    for (i = 0; i < offset_tiles.length; i++) {
-		surround_xy = {"x": grid_xy.x + offset_tiles[i].x, "y": grid_xy.y + offset_tiles[i].y};
-		if (isValidGridCoordinates(surround_xy) === false) {
-		    continue;
-		}
-		surrounding_tiles.push(surround_xy);
-	    }
-	    
-	    return surrounding_tiles;
-	},
-	
-	// Methods that do actual drawing
-	///////////////////////////////////////////////////////////////////////
-	
-	drawImageAt: function (grid_xy, image) {
-	// place an image on a grid tile based on its coordinates
-	    my_context.drawImage(image, grid_xy.x * tile_width, grid_xy.y * tile_height, tile_width, tile_height);
-	},
-
-	drawBorderAt: function (grid_xy, stroke_style) {
-	// draw simple border around a grid tile based on its coordinates
-	
-	    my_context.strokeStyle = stroke_style;
-	    my_context.strokeRect(grid_xy.x * tile_width, grid_xy.y * tile_height, tile_width, tile_height);
-	},
-	
-	drawFillAt: function (grid_xy, fill_style) {
-	    my_context.fillStyle = fill_style;
-	    my_context.fillRect(grid_xy.x * tile_width, grid_xy.y * tile_height, tile_width, tile_height);
-	},
-	
-	// Accessor Methods
-	///////////////////////////////////////////////////////////////////////
-	getWidth: function ( ) {
-	    return width;
-	},
-	
-	getHeight: function ( ) {
-	    return height;
-	},
-	
-	getCurrentGrid: function ( ) {
-	    return current_xy;
-	},
-	
-	getCanvasContext: function ( ) {
-	    return my_context;
+	if (isValidGridCoordinates(new_xy) === false) {
+	    return null;
+	} else {
+	    return new_xy;
 	}
     };
+    
+    that.getTilesFromOffsetArray = function (grid_xy, offset_array) {
+	// generates an array of grid coordinates based on a start point and an array of offsets
+	// any grid points that are invalid will not be present in the array
+    
+	var new_xy, i;
+	var new_grids = [];
+    
+	// is offset an array?
+	if (isArray(offset_array) === false) {
+	    throw {
+		'name': 'InvalidArgumentType',
+		'message': 'offset_array needs to be an actual array'
+	    };
+	}
+	
+	for (i = 0; i < offset_array.length; i += 1) {
+	    new_xy = this.getTileFromOffset(grid_xy, offset_array[i]);
+	    if (new_xy !== null) {
+		new_grids.push(new_xy);
+	    }
+	}
+	
+	return new_grids;
+    };
+    
+    that.getSurroundingTiles = function (grid_xy, b_include_center) {
+    // returns an array of {x: , y: } coordinates surrounding a given (center) coordinate
+    // will optionally include the center if the 2nd argument is passed as true
+    // IGNORES invalid coordinates (e.g. off the map)
+	
+	var offset_tiles = [
+	    {"x": -1, "y": -1}, {"x": -1, "y": 0},
+	    {"x": -1, "y": 1}, {"x": 0, "y": -1},
+	    {"x": 0, "y": 1}, {"x": 1, "y": -1}, 
+	    {"x": 1, "y": 0}, {"x": 1, "y": 1}
+	];
+		
+	if (b_include_center === true) {
+	    offset_tiles.push({"x": 0, "y": 0});
+	}
+	
+	return this.getTilesFromOffsetArray(grid_xy, offset_tiles);
+    };
+    
+    that.getCardinalTiles = function (grid_xy, b_include_center) {
+    // returns an array of {x: , y: } coordinates adjacent to a given (center) coordinate
+    // will optionally include the center if the 2nd argument is passed as true
+    // IGNORES invalid coordinates (e.g. off the map)
+    
+	var offset_tiles = [
+	    {"x": -1, "y": 0}, {"x": 0, "y": -1},
+	    {"x": 0, "y": 1}, {"x": 1, "y": 0}
+	];
+		
+	if (b_include_center === true) {
+	    offset_tiles.push({"x": 0, "y": 0});
+	}
+	
+	return this.getTilesFromOffsetArray(grid_xy, offset_tiles);
+    };
+
+    that.getContextCoordinates = function (grid_xy) {
+    // returns the actual context coordinates for a given grid
+	return {"x": grid_xy.x * my_tile_width, "y": grid_xy.y * my_tile_height};
+    };
+    
+    that.getContextRectangle = function (grid_xy) {
+    // returns the actual context rectangle (x, y, width, height) for a given grid
+	return {"x": grid_xy.x * my_tile_width, "y": grid_xy.y * my_tile_height, "width": my_tile_width, "height": my_tile_height};
+    };
+    
+    // Methods that do actual drawing
+    ///////////////////////////////////////////////////////////////////////
+    
+    that.drawImageAt = function (grid_xy, image) {
+    // place an image on a grid tile based on its coordinates
+	my_context.drawImage(image, grid_xy.x * my_tile_width, grid_xy.y * my_tile_height, my_tile_width, my_tile_height);
+    };
+
+    that.drawBorderAt = function (grid_xy, stroke_style) {
+    // draw simple border around a grid tile based on its coordinates
+    
+	my_context.strokeStyle = stroke_style;
+	my_context.strokeRect((grid_xy.x * my_tile_width) + 1, (grid_xy.y * my_tile_height) + 1, my_tile_width - 2, my_tile_height - 2);
+    };
+    
+    that.drawFillAt = function (grid_xy, fill_style) {
+	my_context.fillStyle = fill_style;
+	my_context.fillRect(grid_xy.x * my_tile_width, grid_xy.y * my_tile_height, my_tile_width, my_tile_height);
+    };
+    
+    that.clearAt = function (grid_xy) {
+	my_context.clearRect(grid_xy.x * my_tile_width, grid_xy.y * my_tile_height, my_tile_width, my_tile_height);
+    };
+    
+    // Accessor Methods
+    ///////////////////////////////////////////////////////////////////////
+    that.getWidth = function ( ) {
+	return my_width;
+    };
+    
+    that.getHeight = function ( ) {
+	return my_height;
+    };
+    
+    that.getTileHeight = function ( ) {
+	return my_tile_height;
+    };
+    
+    that.getTileWidth = function ( ) {
+	return my_tile_width;
+    };
+    
+    that.getCurrentGrid = function ( ) {
+	return my_current_xy;
+    };
+    
+    that.getLastClickGrid = function ( ) {
+	return my_last_click_xy;
+    };
+
+    that.getLastVisitGrid = function ( ) {
+	return my_last_visit_xy;
+    };
+    
+    that.getCanvasContext = function ( ) {
+	return my_context;
+    };
+    
+
+    // add these last
+    my_canvas.addEventListener("mousedown", canvasOnMousedown, false);
+    my_canvas.addEventListener("mousemove", canvasOnMousemove, false);
+    
+    // this is the actual grid mangler object with public methods    
+    return that;
 };
-
-
-
-        
-        
-        
-        
-            
